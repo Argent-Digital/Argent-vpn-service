c0131a3f863158afa0e8938424d7da31df773b44
platon <platongimp96@gmail.com>
2026-06-05 23:10:02 +0500

    feat: docker image

--- src/main.py
+++ src/main.py
@@ -24,4 +24,4 @@ async def health_check():
     return {"status": "working", "service": "argent-vpn"}
 
 if __name__ == "__main__":
-    uvicorn.run("src.main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
